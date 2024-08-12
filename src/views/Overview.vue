826c7954467486ae7a7936ebdb56ef9046333b8a
Luke O'Leary <luke.oleary1@gmail.com>
2024-08-12 09:40:46 +0100

    feat: added auto updating

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -9,6 +9,7 @@
             :hospitalId="hospital.id"
             :title="hospital.description"
             :showDataLabels="true"
+            :update="update"
         />
     </div>
     <cv-tile v-else>
@@ -22,6 +23,11 @@ import HospitalCapacityDistribution from "@/components/charts/HospitalCapacityDi
 
 export default {
     name: "Overview",
+    data() {
+        return {
+            update: 0
+        };
+    },
     components: {
         HospitalAvailability,
         HospitalCapacityDistribution
@@ -32,6 +38,32 @@ export default {
         },
         disabledReasons() {
             return this.$store.getters.getDisabledReasons;
+        },
+        userDetails() {
+            return this.$store.getters.getUserDetails;
+        }
+    },
+    methods: {
+        autoFetch() {
+            if (!this.fetchInterval) {
+                console.info("Automatically fetching data every 5 minutes.");
+                this.fetchInterval = setInterval(() => {
+                    this.update++;
+                }, 300000);
+            }
+        }
+    },
+    watch: {
+        userDetails() {
+            this.autoFetch();
+        }
+    },
+    mounted() {
+        this.autoFetch();
+    },
+    beforeDestroy() {
+        if (this.fetchInterval) {
+            clearInterval(this.fetchInterval);
         }
     }
 };
