73e4e29405370b15c4efc0536874a60bc315a672
Luke O'Leary <luke.oleary1@gmail.com>
2024-08-12 09:17:07 +0100

    fix: style issues and added v-for loop for non hardcoded functionality

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,90 +1,56 @@
 <template>
-  <div class="container">
-    <div class="bar-chart-container">
-      <BarChart />
+    <div class="overview" v-if="disabledReasons.length > 0">
+        <cv-tile>
+            <hospital-capacity-distribution :hospitals="hospitals" />
+        </cv-tile>
+        <hospital-availability
+            v-for="hospital in hospitals"
+            :key="hospital.id"
+            :hospitalId="hospital.id"
+            :title="hospital.description"
+            :showDataLabels="true"
+        />
     </div>
-    <div class="bar-chart-container2">
-      <BarChartId2/>
-    </div>
-    <div class="bar-chart-container3">
-      <BarChartId3/>
-    </div>
-    <div class="bar-chart-container3">
-      <BarChartId4/>
-    </div>
-    <div class="bar-chart-container3">
-      <BarChartId5/>
-    </div>
-    <div class="bar-chart-container3">
-      <BarChartId6/>
-    </div>
-    <div class="pie-chart-container">
-      <PieChart />
-    </div>
-  </div>
+    <cv-tile v-else>
+        <cv-inline-loading state="loading" />
+    </cv-tile>
 </template>
 
 <script>
-import BarChart from "@/components/charts/BarChart.vue";
-import PieChart from "@/components/charts/PieChart.vue";
-import BarChartId2 from "@/components/charts/BarChartId2.vue";
-import BarChartId3 from "@/components/charts/BarChartId3.vue";
-import BarChartId4 from "@/components/charts/BarChartId4.vue";
-import BarChartId5 from "@/components/charts/BarChartId5.vue";
-import BarChartId6 from "@/components/charts/BarChartId6.vue";
+import HospitalAvailability from "@/components/charts/HospitalAvailability.vue";
+import HospitalCapacityDistribution from "@/components/charts/HospitalCapacityDistribution.vue";
 
 export default {
-  name: "Overview",
-  components: {
-    BarChart,
-    PieChart,
-    BarChartId2,
-    BarChartId3,
-    BarChartId4,
-    BarChartId5,
-    BarChartId6
-  },
+    name: "Overview",
+    components: {
+        HospitalAvailability,
+        HospitalCapacityDistribution
+    },
+    computed: {
+        hospitals() {
+            return this.$store.getters.allHospitals;
+        },
+        disabledReasons() {
+            return this.$store.getters.getDisabledReasons;
+        }
+    }
 };
 </script>
 
 <style scoped lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap");
-
-
-.container {
-  display: grid;
-  grid-template-columns: repeat(3,1fr); 
-  gap: 20px;
-  font-family: "Roboto", sans-serif;
-  padding: 30px;
-  // background-color: #5b13d6;
+.overview {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    height: 100vh;
 }
-
-.pie-chart-container
- {
-  background-color: #ffffff;
-  flex: 1;
-  max-width: 400px; 
-  margin: 0 20px;
-  padding: 20px;
+.cv-tile {
+    margin: 1em;
+    width: 23%;
+    min-width: 20em;
+    height: 40%;
 }
-
-.bar-chart-container
-{
-  background-color: #ffffff;
-  flex: 1;
-  max-width: 450px;
-  min-height: 200px; 
-  margin: 0 20px;
-  border-radius: 8px;
-  
-}
-
-.my-chart-id {
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 8px;
-  
-}
-
 </style>
